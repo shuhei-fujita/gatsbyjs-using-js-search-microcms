1cfa1128e3f404933bacbcaa39aab966d4aa8613
syuheifujita <syuheifujita0117@gmail.com>
2021-11-01 16:34:26 +0900

    error 回避

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -16,7 +16,7 @@ class Search extends Component {
         Axios.get(`https://bvaughn.github.io/js-search/books.json`)
             .then(result => {
                 const newsData = result.data
-                this.setState({ newsList: newsData.news })
+                this.setState({ newsList: newsData.books })
                 this.rebuildIndex()
             })
             .catch(err => {
@@ -29,47 +29,47 @@ class Search extends Component {
 
     rebuildIndex = () => {
         const { newsList } = this.state
-    
+
         const dataToSearch = new JsSearch.Search(`isbn`)
-    
+
         /**
          *  defines a indexing strategy for the data
          * more more about it in here https://github.com/bvaughn/js-search#configuring-the-index-strategy
          */
         dataToSearch.indexStrategy = new JsSearch.PrefixIndexStrategy()
-    
+
         /**
          * defines the sanitizer for the search
          * to prevent some of the words from being excluded
          *
          */
         dataToSearch.sanitizer = new JsSearch.LowerCaseSanitizer()
-    
+
         /**
          * defines the search index
          * read more in here https://github.com/bvaughn/js-search#configuring-the-search-index
          */
         dataToSearch.searchIndex = new JsSearch.TfIdfSearchIndex(`isbn`)
-    
+
         dataToSearch.addIndex(`title`) // sets the index attribute for the data
         dataToSearch.addIndex(`author`) // sets the index attribute for the data
-    
+
         dataToSearch.addDocuments(newsList) // adds the data to be searched
         this.setState({ search: dataToSearch, isLoading: false })
-      }
-    
-      /**
-       * handles the input change and perform a search with js-search
-       * in which the results will be added to the state
-       */
-      searchData = e => {
+    }
+
+    /**
+     * handles the input change and perform a search with js-search
+     * in which the results will be added to the state
+     */
+    searchData = e => {
         const { search } = this.state
         const queryResult = search.search(e.target.value)
         this.setState({ searchQuery: e.target.value, searchResults: queryResult })
-      }
-      handleSubmit = e => {
+    }
+    handleSubmit = e => {
         e.preventDefault()
-      }
+    }
 
     render() {
         const {
@@ -81,8 +81,31 @@ class Search extends Component {
         } = this.state
         const queryResults = searchQuery === `` ? newsList : searchResults
 
-        if (isLoading) { }
-        if (isError) { }
+        if (isLoading) {
+            return (
+                <div style={{ margin: `1.2rem 1rem 1.2rem 1rem` }}>
+                    <h1 style={{ marginTop: `3em`, textAlign: `center` }}>
+                        Getting the search all setup
+                    </h1>
+                </div>
+            )
+        }
+        if (isError) {
+            return (
+                <div style={{ margin: `1.2rem 1rem 1.2rem 1rem` }}>
+                    <h1 style={{ marginTop: `3em`, textAlign: `center` }}>Ohh no!!!!!</h1>
+                    <h3
+                        style={{
+                            marginTop: `2em`,
+                            padding: `2em 0em`,
+                            textAlign: `center`,
+                        }}
+                    >
+                        Something really bad happened
+                    </h3>
+                </div>
+            )
+        }
 
         return (
             <div>
