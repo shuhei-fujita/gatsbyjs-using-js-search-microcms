b4ef3e5673936be8a2c9a603e14262ec9befc57f
syuheifujita <syuheifujita0117@gmail.com>
2021-11-01 16:46:42 +0900

    コメントアウト

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -215,25 +215,6 @@ class Search extends Component {
             </div>
         )
     }
-
-    // getOrCreateIndex = () =>
-    //     this.index
-    //         ? this.index
-    //         : // Create an elastic lunr index and hydrate with graphql query results
-    //         Index.load(this.props.searchIndex)
-
-    // search = evt => {
-    //     const query = evt.target.value
-    //     this.index = this.getOrCreateIndex()
-    //     this.setState({
-    //         query,
-    //         // Query the index with search string to get an [] of IDs
-    //         results: this.index
-    //             .search(query, {})
-    //             // Map over each ID and return the full document
-    //             .map(({ ref }) => this.index.documentStore.getDoc(ref)),
-    //     })
-    // }
 }
 
 export default Search
